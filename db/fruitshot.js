898c9bd95076d42f0c0053d292b1ae60191a2f59
Utkrishta Bhattarai <bhattarai.utk1@gmail.com>
2020-06-27 10:57:34 +0545

    first commit

---
+++ db/fruitshot.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/fruitshot', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+})
+    .then((db) => {
+        console.log("Successfully connected to MongodB server");
+    }, (err) => console.log(err));

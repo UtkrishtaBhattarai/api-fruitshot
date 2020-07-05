75691fb6cdeb99820dce44b49dd399c96404203a
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2020-07-05 20:31:35 +0545

    Notification is now added

--- app.js
+++ app.js
@@ -10,17 +10,18 @@ const registerRoute = require("./routes/users");
 const productRouter = require("./routes/products");
 const uploadRouter = require("./routes/upload_route");
 const SliderRoute = require("./routes/slider_route");
-const CartRoute = require('./routes/cart');
-const OrderRoute = require('./routes/order');
-const emailRoute=require('./routes/nodemailer');
+const CartRoute = require("./routes/cart");
+const OrderRoute = require("./routes/order");
+const emailRoute = require("./routes/nodemailer");
+const notificationRoute = require("./routes/notification");
 
 var app = express();
 app.use(function (req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-	res.setHeader("Access-Control-Allow-Credentials", true);
-	next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -28,9 +29,9 @@ app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(
-	bodyparser.urlencoded({
-		extended: false,
-	})
+  bodyparser.urlencoded({
+    extended: false,
+  })
 );
 
 require("./db/fruitshot");
@@ -42,36 +43,37 @@ app.use("/register", registerRoute);
 app.use("/upload", uploadRouter);
 app.use("/product", productRouter);
 app.use("/slider", SliderRoute);
-app.use('/cart', CartRoute);
-app.use('/order', OrderRoute);
-app.use('/email',emailRoute);
+app.use("/cart", CartRoute);
+app.use("/order", OrderRoute);
+app.use("/email", emailRoute);
+app.use("/notification", notificationRoute);
 app.use(auth.verifyUser);
 
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/fruitshot";
 
-MongoClient.connect(url, function(err, db) {
-
+MongoClient.connect(url, function (err, db) {
   if (err) throw err;
-  let password1="admin123";
-  bcrypt.hash(password1, 10, function(err, hash) {
+  let password1 = "admin123";
+  bcrypt.hash(password1, 10, function (err, hash) {
     if (err) {
       throw new Error("Could not hash!");
-	}
-	
-  var dbo = db.db("fruitshot");
-  var myobj = { email: "admin123@gmail.com",password:hash};
-  dbo.collection("admin_logins").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
+    }
+
+    var dbo = db.db("fruitshot");
+    var myobj = { email: "admin123@gmail.com", password: hash };
+    dbo.collection("admin_logins").insertOne(myobj, function (err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
   });
-})});
+});
 
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.statusCode = 500;
-	res.json({ status: err.message });
+  console.error(err.stack);
+  res.statusCode = 500;
+  res.json({ status: err.message });
 });
 
 app.listen(4000);

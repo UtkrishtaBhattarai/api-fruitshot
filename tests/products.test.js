a99a79ea455b2b13f56cb8786c62837a203acea7
Utkrishta Bhattarai <bhattarai.utk1@gmail.com>
2020-07-05 17:01:04 +0545

    tests added for notification

--- tests/products.test.js
+++ tests/products.test.js
@@ -1,31 +1,47 @@
-//path of the model here
-//as the developmemt is TDD we need to do testing first
-const Users=require('../models/users');
-const mongoose=require('mongoose');
+// use the path of your model
+const Product = require("../models/product");
+const mongoose = require("mongoose");
+// use the new name of the database
+const url = "mongodb://localhost:27017/spare_api_test";
+beforeAll(async () => {
+  await mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+  });
+});
 
-//new name of database
-const url='mongodb://localhost:27017/test_database';
-beforeAll(async()=> {
-    await mongoose.connect(url,{
-        useNewUrlParser:true,
-        useCreateIndex:true,
-        useUnifiedTopology:true
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+describe("Product Schema test", () => {
+  // the code below is for insert testing
+  it("Add product testing anything", () => {
+    const product = {
+      name: "Bike Chasis",
+      price: 1233
+    };
+    return Product.create(product).then(pro_ret => {
+      expect(pro_ret.name).toEqual("Bike Chasis");
     });
+  });
+
+  it("to test the update", async () => {
+    return product
+      .findOneAndUpdate(
+        { _id: Object("5e479758fae5043858633ff8") },
+        { $set: { name: "chasis" } }
+      )
+      .then(product => {
+        expect(product.name).toEqual("chasis");
+      });
+  });
+
+  //the code below is for delete testing
+  it("to test the delete register is working or not", async () => {
+    const status = await Register.deleteMany();
+    expect(status.ok).toBe(1);
+  });
 });
-
-afterAll(async()=>{
-    await mongoose.connection.close();
-});
-
-describe(' Product Schema Test',()=>{
-    //for insert testing
-    it('Add Product',()=>{
-        const user={
-            'name':'TP2USB',
-            'price':'12',
-            'usage':'very nice'
-        }
-    })
-
-})
-

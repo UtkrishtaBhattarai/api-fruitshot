e7d5d94beec4509901811d2a63627fb55cf57665
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2020-08-02 23:38:27 +0545

    modifycomment

--- models/comment.js
+++ models/comment.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Comment = mongoose.Schema({
 
+    //comment model
     userid: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'register',

545036350e41f63e2393542a4e5720c8a95ac9d1
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2020-08-10 18:53:30 +0545

    modified

--- models/product.js
+++ models/product.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const productSchema = mongoose.Schema({
-    image: String,
-    name: String,
-    price: Number,
-    description: String,
-    usage: String
+  //product model
 
+  image: String,
+  name: String,
+  price: Number,
+  description: String,
+  usage: String,
 });
-module.exports = mongoose.model('products', productSchema);
+module.exports = mongoose.model("products", productSchema);
--- models/users.js
+++ models/users.js
@@ -2,28 +2,29 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
 const Register = new mongoose.Schema({
-	fname: {
-		type: String
-	},
-	lname: {
-		type: String
-	},
-	address: {
-		type: String
-	},
-	phone: {
-		type: String
-	},
-	email: {
-        type: String,
-        required:true    
-	},
-	password: {
-		type: String
-	},
-	createddate:{
-		type:Date
-	}
+  //user model
+  fname: {
+    type: String,
+  },
+  lname: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+  },
+  createddate: {
+    type: Date,
+  },
 });
 
 module.exports = mongoose.model("register", Register);

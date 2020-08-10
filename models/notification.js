de477cef47cdce27970b09913ab772eca6dd9f0d
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2020-08-10 18:52:26 +0545

    notification update

--- models/notification.js
+++ models/notification.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const Notification = mongoose.Schema({
+  //like sidlike model updated
+
   postedDate: Date,
   endDate: Date,
   title: String,
-  description: String
+  description: String,
 });
 module.exports = mongoose.model("notification", Notification);
